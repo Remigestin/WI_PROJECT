86b34a25911857ca4315f6ff49c801abee38b5dd
GardeisenChawaf <marine.gardeisen@free.fr>
2019-11-01 21:43:37 +0100

    linear regression

--- src/main/scala/FirstSparkApplication.scala
+++ src/main/scala/FirstSparkApplication.scala
@@ -2,7 +2,7 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{DecisionTreeClassifier, LogisticRegression}
 import org.apache.spark.ml.evaluation.{MulticlassClassificationEvaluator, RegressionEvaluator}
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler, VectorIndexer}
-import org.apache.spark.ml.regression.GBTRegressor
+import org.apache.spark.ml.regression.{GBTRegressor, LinearRegression,LinearRegressionModel}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.IntegerType
 import org.apache.spark.mllib.tree.DecisionTree
@@ -52,46 +52,59 @@ object FirstSparkApplication extends App {
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // Index labels, adding metadata to the label column.
   // Fit on whole dataset to include all labels in index.
+
   val df2 = finalJson.selectExpr("cast(label AS STRING) label",
     "bidFloor",
     "media")
+
+  // Split the data into training and test sets (30% held out for testing)
+  val Array(trainingData, testData) = df2.randomSplit(Array(0.7, 0.3))
 
   val labelIndexer = new StringIndexer()
     .setInputCol("label")
     .setOutputCol("indexedLabel")
     .fit(df2)
 
+
   //We define a StringIndexers for the categorical variable media
   val indexerMedia = new StringIndexer()
     .setInputCol("media")
     .setOutputCol("mediaIndex")
+    .fit(df2)
+
 
   val assembler = new VectorAssembler()
     .setInputCols(Array("bidFloor", "mediaIndex"))
     .setOutputCol("features")
 
-  // Split the data into training and test sets (30% held out for testing)
-  val Array(trainingData, testData) = df2.randomSplit(Array(0.7, 0.3))
 
   // Train a DecisionTree model.
-  val dt = new DecisionTreeClassifier()
-    .setLabelCol("indexedLabel")
+  val lr = new LinearRegression()
+    .setLabelCol("label")
     .setFeaturesCol("features")
 
+
+
   // Convert indexed labels back to original labels.
-  val labelConverter = new IndexToString()
+ /* val labelConverter = new IndexToString()
     .setInputCol("prediction")
     .setOutputCol("predictedLabel")
     .setLabels(labelIndexer.labels)
+*/
 
   // Chain indexers and tree in a Pipeline
-  val pipeline = new Pipeline().setStages(Array(labelIndexer,indexerMedia, assembler, dt, labelConverter))
-
+  val pipeline = new Pipeline().setStages(Array(labelIndexer,indexerMedia, assembler, lr))
+  //val pipeline = new Pipeline().setStages(Array(indexerMedia, assembler, lr))
   // Train model.  This also runs the indexers.
   val model = pipeline.fit(trainingData)
 
+
+  //Show model results
+  val linRegModel = model.stages(1).asInstanceOf[LinearRegressionModel]
+  linRegModel.summary.residuals.show()
+
   // Make predictions.
-  val predictions = model.transform(testData)
+  /*val predictions = model.transform(testData)
 
   // Select example rows to display.
   predictions.select("predictedLabel", "label", "features").show(5)
@@ -103,7 +116,7 @@ object FirstSparkApplication extends App {
     .setMetricName("precision")
   val accuracy = evaluator.evaluate(predictions)
   println("Test Error = " + (1.0 - accuracy))
-
+*/
   spark.stop
 
 }
